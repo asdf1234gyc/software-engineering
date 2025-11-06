0d6af15dc04023002ccbda9b2c113e169b1b012a
asdf1234gyc <1483875459@qq.com>
2025-11-06 16:58:04 +0800

    Add files via upload

---
+++ t4/tutorial4/tutorial4-main/src/test/java/org/example/CalcTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalcTest {
+  Calc c = new Calc();
+  Calc d=new Calc();
+
+    @Test
+    void testAddition() {
+        assertEquals(4, c.add(2,2));
+    }
+    @Test
+    void testSubtraction(){assertEquals(2, d.subtract(4,2));}
+
+}
